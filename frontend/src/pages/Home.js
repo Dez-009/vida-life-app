7ac61f146d241b1ba66ac506a40dc236ba0d736b
Dez_the_Dev <bshcontracts@proton.me>
2025-07-07 00:35:38 -0400

    Add React Router navigation

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Home() {
+  return <div className="page-content"><h1>Welcome to Vida Life Coach</h1></div>;
+}
+export default Home;
---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Profile() {
+  return <div className="page-content"><h1>Your Profile Page</h1></div>;
+}
+export default Profile;
